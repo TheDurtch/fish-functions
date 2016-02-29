face8c78cc0459115fa91556c4b4d64d78cb7166
YandereKate <TheDurtch@gmail.com>
2016-02-29 01:33:02 -0800

    Removed useless SPAM echos Added CPU_COUNT Added missing lines that let you continue without looking at a changelog.

--- post-kernel-update.fish
+++ post-kernel-update.fish
@@ -1,9 +1,8 @@
 function post-kernel-update
+set CPU_COUNT (math (cat /proc/cpuinfo | grep processor | wc -l)+1)
 echo "Initilize sudo"
 sudo echo "sudo initilized"
-echo "Changing to /kernel-update-stuff/"
 cd /kernel-update-stuff/
-echo "Getting netatop variables"
 set natopver1 (curl -s http://www.atoptool.nl/downloadnetatop.php  | grep "netatop-0...tar.gz" | sed -n '1p' | cut -d '"' -f2 | cut -d "." -f1-2 | cut -d "/" -f2)
 set natopver2 (curl -s http://www.atoptool.nl/downloadnetatop.php  | grep "netatop-0...tar.gz" | sed -n '2p' | cut -d '"' -f2 | cut -d "." -f1-2 | cut -d "/" -f2)
 set natopver3 (curl -s http://www.atoptool.nl/downloadnetatop.php  | grep "netatop-0...tar.gz" | sed -n '3p' | cut -d '"' -f2 | cut -d "." -f1-2 | cut -d "/" -f2)
@@ -16,34 +15,26 @@ read prompt1
 if [ "$prompt1" = "1" ]
 set natopver $natopver1
 mv -rf $natopver0/ old/
-echo "Downloading and unpacking netatop"
 curl http://www.atoptool.nl/download/$natopver.tar.gz | tar xzf -
 end
 if [ "$prompt1" = "2" ]
 set natopver $natopver2
 mv -rf $natopver0/ old/
-echo "Downloading and unpacking netatop"
 curl http://www.atoptool.nl/download/$natopver.tar.gz | tar xzf -
 end
 if [ "$prompt1" = "3" ]
 set natopver $natopver3
 mv -rf $natopver0/ old/
-echo "Downloading and unpacking netatop"
 curl http://www.atoptool.nl/download/$natopver.tar.gz | tar xzf -
 end
 if [ "$prompt1" = "0" ]
 set natopver $natopver0
 end
-echo "Changing to netatop build dir"
 sudo chown -Rc kate:users $natopver
 cd /kernel-update-stuff/$natopver
-echo "Building netatop"
-make
-echo "Installing netatop"
+make -j$CPU_COUNT
 sudo make install
-echo "Changing to nVidia Display Drivers dir"
 cd /kernel-update-stuff/nvidia-drivers/
-echo "Setting nvidiaver variable"
 curl -s http://www.nvidia.com/object/linux-amd64-display-archive.html > /tmpfs/NVIDIA_LINUX_SOUCE_HTML
 set nvidiaver1 (cat /tmpfs/NVIDIA_LINUX_SOUCE_HTML | grep "Download/" | sed -n '3p' | cut -d ':' -f3 | cut -d ' ' -f2 | cut -d '<' -f1)
 set nvidiachangelogurl1 (cat /tmpfs/NVIDIA_LINUX_SOUCE_HTML | grep "Download/" | sed -n '3p' | cut -d '"' -f4)
@@ -56,9 +47,11 @@ echo "You have versions $nvidiaver1, $nvidiaver2, and $nvidiaver3 available onli
 You have version $nvidiaver0 on your computer already.
 Would you like to check the changelogs for any of the online versions?
 "
+echo "Enter 0 if you want don't need to see the changelog"
 echo "Enter 1 if you want to view the changelog for version $nvidiaver1"
 echo "Enter 2 if you want to view the changelog for version $nvidiaver2"
 echo "Enter 3 if you want to view the changelog for version $nvidiaver3"
+
 read prompt1
 if [ "$prompt1" = "1" ]
 echo $nvidiachangelogurl1
@@ -77,26 +70,23 @@ echo "Oh I see how it is...
 well then..."
 end
 
+echo "Enter 0 if you $nvidiaver0 is the latest version"
 echo "Enter 1 if you want $nvidiaver1"
 echo "Enter 2 if you want $nvidiaver2"
 echo "Enter 3 if you want $nvidiaver3"
 echo "Enter 9 if you don't want to do anything"
-echo "Enter 0 if you $nvidiaver0 is the latest version"
 read prompt1
 if [ "$prompt1" = "1" ]
 set nvidiaver $nvidiaver1
 mv -f NVIDIA-Linux-x86_64-$nvidiaver0.run ../old/
-echo "Downloading nVidia Drivers"
 curl http://us.download.nvidia.com/XFree86/Linux-x86_64/$nvidiaver/NVIDIA-Linux-x86_64-$nvidiaver.run -o NVIDIA-Linux-x86_64-$nvidiaver.run
 end
 if [ "$prompt1" = "2" ]
 set nvidiaver $nvidiaver2
-echo "Downloading nVidia Drivers"
 curl http://us.download.nvidia.com/XFree86/Linux-x86_64/$nvidiaver/NVIDIA-Linux-x86_64-$nvidiaver.run -o NVIDIA-Linux-x86_64-$nvidiaver.run
 end
 if [ "$prompt1" = "3" ]
 set nvidiaver $nvidiaver3
-echo "Downloading nVidia Drivers"
 curl http://us.download.nvidia.com/XFree86/Linux-x86_64/$nvidiaver/NVIDIA-Linux-x86_64-$nvidiaver.run -o NVIDIA-Linux-x86_64-$nvidiaver.run
 end
 if [ "$prompt1" = "0" ]
@@ -108,13 +98,8 @@ Ok I'll just kill this script then.
 It's not like I wanted to run or anything" | randtype -m 4
 exit 0
 end
-echo "Installing nVidia Display Drivers"
-sudo sh NVIDIA-Linux-x86_64-$nvidiaver.run -a --no-questions --disable-nouveau --run-nvidia-xconfig
-echo "Building initrd"
+sudo sh NVIDIA-Linux-x86_64-$nvidiaver.run -a --no-questions --disable-nouveau --run-nvidia-xconfig --concurrency-level=$CPU_COUNT #-s
 sudo mkinitrd
-echo "Probing module nvidia"
 sudo nvidia-modprobe
-echo "changing to init level 5"
 sudo init 5
-echo "Done with script"
 end
