ecf64d07bbf126875a334fbc0fdbbcfa0c0b06ef
YandereKate <TheDurtch@gmail.com>
2016-02-29 16:19:58 -0800

    Rearranged order so prompts came first.

--- post-kernel-update.fish
+++ post-kernel-update.fish
@@ -5,7 +5,7 @@ set USER_GROUP (echo (id -u -n):(id -g -n))
 echo "Initilize sudo"
 sudo echo "sudo initilized"
 cd /kernel-update-stuff/
-##If you don't have atop or don't want to install netatop kernel module then you can start your cut here.
+
 curl -s http://www.atoptool.nl/downloadnetatop.php > /kernel-update-stuff/tmp/NETATOP_LINUX_SOURCE_HTML
 set natopver1 (cat /kernel-update-stuff/tmp/NETATOP_LINUX_SOURCE_HTML | grep "netatop-0...tar.gz" | sed -n '1p' | cut -d '"' -f2 | cut -d "." -f1-2 | cut -d "/" -f2)
 set natopver2 (cat /kernel-update-stuff/tmp/NETATOP_LINUX_SOURCE_HTML | grep "netatop-0...tar.gz" | sed -n '2p' | cut -d '"' -f2 | cut -d "." -f1-2 | cut -d "/" -f2)
@@ -16,34 +16,7 @@ echo "Enter 2 if you want $natopver2"
 echo "Enter 3 if you want $natopver3"
 echo "Enter 0 if $natopver0 is the latest version"
 read prompt1
-if [ "$prompt1" = "1" ]
-set natopver $natopver1
-set download_ATOP 1
-end
-if [ "$prompt1" = "2" ]
-set natopver $natopver2
-set download_ATOP 1
-end
-if [ "$prompt1" = "3" ]
-set natopver $natopver3
-set download_ATOP 1
-end
-if [ "$prompt1" = "0" ]
-set natopver $natopver0
-set download_ATOP 0
-end
-if [ "$download_ATOP" = "1"]
-mv -rf $natopver0/ old/
-curl http://www.atoptool.nl/download/$natopver.tar.gz | tar xzf -
-end
-if [ "$download_ATOP" = "0"]
-end
-sudo chown -Rc $USER_GROUP $natopver
-cd /kernel-update-stuff/$natopver
-make -j$CPU_COUNT
-sudo make install
-##END CUT HERE
-cd /kernel-update-stuff/nvidia-drivers/
+
 curl -s http://www.nvidia.com/object/linux-amd64-display-archive.html > /kernel-update-stuff/tmp/NVIDIA_LINUX_SOUCE_HTML
 set nvidiaver1 (cat /kernel-update-stuff/tmp/NVIDIA_LINUX_SOUCE_HTML | grep "Download/" | sed -n '3p' | cut -d ':' -f3 | cut -d ' ' -f2 | cut -d '<' -f1)
 set nvidiachangelogurl1 (cat /kernel-update-stuff/tmp/NVIDIA_LINUX_SOUCE_HTML | grep "Download/" | sed -n '3p' | cut -d '"' -f4)
@@ -60,61 +33,105 @@ echo "Enter 0 if you want don't need to see the changelog"
 echo "Enter 1 if you want to view the changelog for version $nvidiaver1"
 echo "Enter 2 if you want to view the changelog for version $nvidiaver2"
 echo "Enter 3 if you want to view the changelog for version $nvidiaver3"
-
-read prompt1
-if [ "$prompt1" = "1" ]
-echo $nvidiachangelogurl1
-curl -s $nvidiachangelogurl1 | grep 'ul type="disc"' | html2text -utf8
-end
-if [ "$prompt1" = "2" ]
-echo $nvidiachangelogurl2
-curl -s $nvidiachangelogurl2 | grep 'ul type="disc"' | html2text -utf8
-end
-if [ "$prompt1" = "3" ]
-echo $nvidiachangelogurl3
-curl -s $nvidiachangelogurl3 | grep 'ul type="disc"' | html2text -utf8
-end
-if [ "$prompt1" = "0" ]
-echo "Oh I see how it is...
-well then..."
-end
+read prompt2
 
 echo "Enter 0 if you $nvidiaver0 is the latest version"
 echo "Enter 1 if you want $nvidiaver1"
 echo "Enter 2 if you want $nvidiaver2"
 echo "Enter 3 if you want $nvidiaver3"
 echo "Enter 9 if you don't want to do anything"
-read prompt1
+read prompt3
+#############################
 if [ "$prompt1" = "1" ]
+set natopver $natopver1
+set download_ATOP 1
+end
+
+if [ "$prompt1" = "2" ]
+set natopver $natopver2
+set download_ATOP 1
+end
+
+if [ "$prompt1" = "3" ]
+set natopver $natopver3
+set download_ATOP 1
+end
+
+if [ "$prompt1" = "0" ]
+set natopver $natopver0
+set download_ATOP 0
+end
+
+if [ "$download_ATOP" = "1" ]
+mv -rf $natopver0/ old/
+curl http://www.atoptool.nl/download/$natopver.tar.gz | tar xzf -
+end
+if [ "$download_ATOP" = "0" ]
+end
+sudo chown -Rc $USER_GROUP $natopver
+cd /kernel-update-stuff/$natopver
+make -j$CPU_COUNT
+sudo make install
+
+cd /kernel-update-stuff/nvidia-drivers/
+
+if [ "$prompt2" = "1" ]
+echo $nvidiachangelogurl1
+curl -s $nvidiachangelogurl1 | grep 'ul type="disc"' | html2text -utf8
+end
+
+if [ "$prompt2" = "2" ]
+echo $nvidiachangelogurl2
+curl -s $nvidiachangelogurl2 | grep 'ul type="disc"' | html2text -utf8
+end
+
+if [ "$prompt2" = "3" ]
+echo $nvidiachangelogurl3
+curl -s $nvidiachangelogurl3 | grep 'ul type="disc"' | html2text -utf8
+end
+
+if [ "$prompt2" = "0" ]
+echo "Oh I see how it is...
+well then..."
+end
+
+if [ "$prompt3" = "1" ]
 set nvidiaver $nvidiaver1
 mv -f NVIDIA-Linux-x86_64-$nvidiaver0.run ../old/
 set download_NV 1
 end
-if [ "$prompt1" = "2" ]
+
+if [ "$prompt3" = "2" ]
 set nvidiaver $nvidiaver2
 mv -f NVIDIA-Linux-x86_64-$nvidiaver0.run ../old/
 set download_NV 1
 end
-if [ "$prompt1" = "3" ]
+
+if [ "$prompt3" = "3" ]
 set nvidiaver $nvidiaver3
 mv -f NVIDIA-Linux-x86_64-$nvidiaver0.run ../old/
 set download_NV 1
 end
-if [ "$prompt1" = "0" ]
+
+if [ "$prompt3" = "0" ]
 set nvidiaver (ls -la | awk '{print $9}' | grep NVIDIA-Linux-x86_64 | cut -d "-" -f4|cut -d "." -f1-2)
 set download_NV 0
 end
-if [ "$prompt1" = "9" ]
+
+if [ "$prompt3" = "9" ]
 echo "Oh is this only a test??
 Ok I'll just kill this script then.
 It's not like I wanted to run or anything" | randtype -m 4
 exit 0
 end
-if [ "$download_NV" = "1"]
+
+if [ "$download_NV" = "1" ]
 curl http://us.download.nvidia.com/XFree86/Linux-x86_64/$nvidiaver/NVIDIA-Linux-x86_64-$nvidiaver.run -o NVIDIA-Linux-x86_64-$nvidiaver.run
 end
-if [ "$download_NV" = "0"]
+
+if [ "$download_NV" = "0" ]
 end
+
 sudo sh NVIDIA-Linux-x86_64-$nvidiaver.run -a --no-questions --disable-nouveau --run-nvidia-xconfig --concurrency-level=$CPU_COUNT #-s
 rm /kernel-update-stuff/tmp/*
 sudo mkinitrd
